e69285838f65c6367224e76a72f846e3afe60bec
Manas <msawant@users.noreply.github.com>
2023-04-25 20:29:22 -0500

    Updated to Git LFS

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main():
     if user_query != ":q" or user_query != "":
         # st.write(response['gpt_full_response']['choices'][0]['text'])
         response = get_contact_response(user_query)
-        st.write(response[['f_name', 'l_name', 'notes']])
+        st.write(response)
         return
 
 
